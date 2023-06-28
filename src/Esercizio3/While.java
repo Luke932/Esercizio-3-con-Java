b0be0e1fb33bae9723e22d80b6ed3c057726fbe1
Luke932 <126018318+Luke932@users.noreply.github.com>
2023-06-28 15:36:03 +0200

    oook

---
+++ src/Esercizio3/While.java
@@ -0,0 +1,32 @@
+package Esercizio3;
+
+import java.util.Scanner;
+
+public class While {
+	public static void main(String[] args) {
+		Scanner pippo = new Scanner(System.in);
+		String input;
+
+		do {
+			System.out.println("Inserisci una stringa (':q' per uscire): ");
+			input = pippo.nextLine();
+
+			if (!input.equals(":q")) {
+				suddividiStringa(input);
+			}
+		} while (!input.equals(":q"));
+		pippo.close();
+	}
+
+	public static void suddividiStringa(String input) {
+		System.out.println("Suddividi stringa");
+		for (int i = 0; i < input.length(); i++) {
+			char character = input.charAt(i);
+			System.out.println(character);
+			if (i != input.length() - 1) {
+				System.out.println(", ");
+			}
+		}
+		System.out.println();
+	}
+}
