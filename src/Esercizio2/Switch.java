4dcaac935a015f85d2e090997b3a497af94fdd6e
Luke932 <126018318+Luke932@users.noreply.github.com>
2023-06-28 15:17:43 +0200

    ooook

---
+++ src/Esercizio2/Switch.java
@@ -0,0 +1,45 @@
+package Esercizio2;
+
+import java.util.Scanner;
+
+public class Switch {
+
+	public static void main(String[] args) {
+
+		Scanner intero = new Scanner(System.in);
+		System.out.println("Inserisci un numero intero: ");
+		int num = intero.nextInt();
+
+		if (num >= 0 && num <= 3) {
+			String numLet = convertion(num);
+			System.out.println("Il numero " + num + " in lettere è: " + numLet);
+		} else {
+			System.out.println("Errore: il numero deve essere compreso tra 0 e 3");
+		}
+
+		intero.close();
+	}
+
+	public static String convertion(int num) {
+		String numLet;
+
+		switch (num) {
+		case 0:
+			numLet = "zero";
+			break;
+		case 1:
+			numLet = "uno";
+			break;
+		case 2:
+			numLet = "due";
+			break;
+		case 3:
+			numLet = "tre";
+		default:
+			numLet = "numero non valido";
+			break;
+		}
+		return numLet;
+	}
+
+}
